0efa8b4318df12fc1d57b1c8c94e93cf6502ad6e
Volodymyr Ostapiv <3187448+Akceptor@users.noreply.github.com>
2024-09-16 12:02:19 +0300

    Monitors logical level on GPOI5 and sends telegram group chat message

---
+++ ElectricityMonitor.ino
@@ -0,0 +1,66 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+//https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot/archive/master.zip
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+//https://codeload.github.com/taranais/NTPClient/zip/refs/heads/master
+#include <NTPClient.h>
+
+const char* ssid = "MyWifi";
+const char* password = "12345678";
+
+#define BOTtoken "7024081855:AAFvuyT_xbxdSlDSm_eqHUhUNr2XWe9i-i0"
+#define CHAT_ID "-4570930454"
+#define PWR_GPIO 5
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+String formattedDate;
+String timeStamp;
+int State;
+int PrevState;
+
+void setup() {
+  pinMode(PWR_GPIO, INPUT);
+  Serial.begin(115200);  // Initialize serial communication
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  timeClient.begin();
+  timeClient.setTimeOffset(7200);  //GMT+2
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.println(WiFi.localIP());
+  delay(5000);
+  client.setInsecure();  // Bypass SSL for testing purposes
+  bot.sendMessage(CHAT_ID, "😎 Бот готовий: " + WiFi.localIP().toString(), "");
+}
+
+void loop() {
+  State = digitalRead(PWR_GPIO);
+  if (PrevState != State) {
+    //Get time
+    while (!timeClient.update()) {
+      timeClient.forceUpdate();
+    }
+    formattedDate = timeClient.getFormattedDate();
+    int splitT = formattedDate.indexOf("T");
+    timeStamp = formattedDate.substring(splitT + 1, formattedDate.length() - 1);
+    //Message
+    if (State == HIGH) {
+      bot.sendMessage(CHAT_ID, "✅ Світло з`явилося о " + timeStamp +" 👌", "");
+    } else {
+      bot.sendMessage(CHAT_ID, "❌ Світло зникло о " + timeStamp +" 😱", "");
+    }
+    PrevState = State;
+  }
+  delay(5000);
+}
